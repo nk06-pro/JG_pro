7167492bd7d56ede9453208ad3865e852d4d9958
nk06-pro <koung3483@gmail.com>
2026-08-09 15:04:32 +0900

    fix: app.py 내용 복구

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-Flask==3.0.3
-gunicorn==22.0.0
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
